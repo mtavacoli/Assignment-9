064e58760c308b06cbfcf57692c269fd090afd4a
Matin Tavacoli <mtavacoli33@gmail.com>
2023-02-10 11:30:09 -0600

    added some questions to index.js

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input"
+        name: "title"
+        message: "What is the title of your project?"
+        validate: validateInput;
+    };
+    {
+        type: "input"
+        name: "description"
+        message: "Please enter a description of your project."
+        validate: validateInput;
+    };
+    {
+        type: "input"
+        name: "motivation"
+        message: "Provide a short description explaining the what, why, and how of your project."
+        validate: validateInput;
+    };
+    {
+        
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
