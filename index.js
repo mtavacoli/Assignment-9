76e4c1139d662219c0db83ea8a3d457dee14e457
Matin Tavacoli <mtavacoli33@gmail.com>
2023-02-14 11:04:00 -0600

    all code for index.js is written, but still need to include packages for this app

--- index.js
+++ index.js
@@ -21,15 +21,74 @@ const questions = [
         validate: validateInput;
     };
     {
-        
-    }
+        type: "input"
+        name: "installation"
+        message: "What are the steps required to install your project?"
+        validate: validateInput;
+    };
+    {
+        type: "input"
+        name: "usage"
+        message: "Provide instructions and examples for use."
+        validate: validateInput;
+    };
+    {
+        type: "list",
+        name: "license",
+        message: "Please select a license for this project.",
+        choices: [
+            "GNU AGPLv3",
+            "GNU GPLv3",
+            "GNU LGPLv3",
+            "Apache 2.0",
+            "Boost Software 1.0",
+            "MIT",
+            "Mozilla",
+        ],
+        validate: validateInput,
+    },
+    {
+        type: "input"
+        name: "contributions"
+        message: "How can users contribute to your project?"
+        validate: validateInput;
+    };
+    {
+        type: "input"
+        name: "userName"
+        message: "What is your GitHub username?"
+        validate: validateInput;
+    };
+    {
+        type: "input",
+        name: "userEmail",
+        message: "What is your GitHub email address that contributors may contact?",
+        validate: function (value) {
+            if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(value)) {
+                return true;
+            } else {
+                return "Not a valid email address. Please enter a valid email address.";
+            }
+        },
+    },
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    FileSystem.wrtiefile(fileName, generateMarkdown(data), function (err) {
+        if (err) {
+            return console.log(err);
+        }
+    });
+}
 
 // TODO: Create a function to initialize app
 function init() {}
+inquirer.prompt(questions).then((date) => {
+    console.log(JSON.stringify(data, null, " "));
+    data.getLicense = getLicense(data.license);
+    writeToFile("./example/README.md", data);
+});
 
 // Function call to initialize app
 init();
